dae415c820f6673917e08e5967930369bc9acea2
Rashid <rashidberetar@gmail.com>
2023-06-17 16:53:00 +0300

    T2 done

--- S1/T2.py
+++ S1/T2.py
@@ -4,3 +4,12 @@
 
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+number = int(input("Введите трёхзначное число: "))
+
+sum = 0
+while number > 0:
+    sum += number % 10
+    number //= 10
+
+print("Сумма цифр: ", sum)
